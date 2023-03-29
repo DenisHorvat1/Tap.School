6af24e9793dd9e67d5e57d767ba655bf3729c9e5
DenisHorvat1 <denishorvat2002@gmail.com>
2023-03-29 21:21:46 +0300

    Incercare ex 9

---
+++ Tap.School.Api/Migrations/20230329180954_init_migration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tap.School.Api.Migrations
+{
+    public partial class init_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Grades",
+                type: "INTEGER",
+                nullable: true,
+                defaultValue: null); //adaugat
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grades_StudentId",
+                table: "Grades",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grades_Students_StudentId",
+                table: "Grades",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grades_Students_StudentId",
+                table: "Grades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Grades_StudentId",
+                table: "Grades");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Grades");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
